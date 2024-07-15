1d9ed319d7c20468776cf4828970e10aee44631e
Shumminnnnnn <sherrysweet28605520@gmail.com>
2024-07-15 19:06:52 +0800

    finished project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,12 +66,11 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
-
     implementation("androidx.compose.ui:ui:1.6.8")
     implementation( "androidx.compose.material:material:1.6.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("com.google.accompanist:accompanist-permissions:0.28.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
