39296f76fee6cbae04dbf91699745f59df7f7b60
Shumminnnnnn <sherrysweet28605520@gmail.com>
2024-07-15 19:40:48 +0800

    finished project

--- app/src/main/java/com/example/myapplication/EndFilter.kt
+++ app/src/main/java/com/example/myapplication/EndFilter.kt
@@ -3,7 +3,6 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
