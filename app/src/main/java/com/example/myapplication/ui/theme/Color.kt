a1350715225de6e4231b9ce4fe31ecfd604d1f8e
Shumminnnnnn <sherrysweet28605520@gmail.com>
2024-08-01 00:59:45 +0800

    finished project

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -2,11 +2,23 @@ package com.example.myapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//深色背景預設色
+//val Purple80 = Color(0xFFD0BCFF)
+//val PurpleGrey80 = Color(0xFFCCC2DC)
+//val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF9e7cfe)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple01 = Color(0xFF9e7cfe)
+val Purple02= Color(0xFFd6c9fc)
+val Purple03 = Color(0xFFbaa2ff)
 
+val Blue01 = Color(0xFF7eb3f1)
+val Blue02 = Color(0xFFb6d3f6)
+val Blue03 = Color(0xFFb8d5f8)
+
+val Brown01 = Color(0xFFd3c4a9)
+val Brown02 = Color(0xFFe0d5c2)
+val Brown03 = Color(0xFFe1d6c3)
+
+val Pink01 = Color(0xFFec71c9)
+val Pink02 = Color(0xFFf2acdf)
+val Pink03 = Color(0xFFf4aee1)
